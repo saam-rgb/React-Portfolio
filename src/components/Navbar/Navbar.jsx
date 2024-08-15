8c2dfe58611b47e0e10dae1b239f3b9dbff8d22a
saam <saamsallvin@gmail.com>
2024-08-15 12:24:49 +0530

    fix: navbar navlink alignment

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./Navbar.css";
+import logo from "../../assets/images/port-logo.png";
 
 const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark ">
-      <div className="container-fluid d-flex ">
+      <div className="container-fluid d-flex align-items-center justify-content-between">
         <a className="navbar-brand flex-grow-1" href="#">
+          <img src={logo} alt="Saam" width={60 + "px"} className="me-2" />
           Saam.js
         </a>
         <button
@@ -26,26 +28,21 @@ const Navbar = () => {
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">
+              <a className="nav-link" href="#about">
                 About
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">
+              <a className="nav-link" href="#projects">
                 Projects
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">
+              <a className="nav-link" href="#contact">
                 Contact
               </a>
             </li>
           </ul>
-          {/* <form className="d-flex" role="search">
-            <button className="btn" type="submit">
-              Resume
-            </button>
-          </form> */}
         </div>
       </div>
     </nav>
