9ec543642907990b756306226b4ab316d7db52f4
saam <saamsallvin@gmail.com>
2024-08-19 08:50:44 +0530

    fix: logo

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "./Footer.css";
+import logo from "../../assets/images/port-logo.png";
 
 const Footer = () => {
   return (
     <footer>
-      <img src="" alt="Saam" />
+      <img src={logo} alt="Saam" width={60 + "px"} className="my-3" />
 
       <div className="socials ">
         <ul className="d-flex ">
